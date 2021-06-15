77bd565236a5c563ca08acd4b279f8edc40af6e4
sytnic <sytnic@list.ru>
2021-06-15 11:09:30 +0300

    Folder routes added

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,47 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// чтобы не указывать отдельно get, post , delete.. для маршрутов, 
+//   можно указать одной строкой:
+//   указываются ресурс (uri, controller), имя маршрута names
+//Route::resource('rest', 'RestTestController')->names('restTest');
+// имя маршрута names('restTest'); можно не задавать
 
+Auth::routes();
+// регистрация новых маршрутов авторизации
 Route::get('/home', 'HomeController@index')->name('home');
+// маршрут в случае успешной авторизации
+
+Route::group(['namespace'=>'Blog','prefix'=>'blog'], function(){
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
+// для отображения индексного маршрута будет работать /blog/posts/ в адресной строке браузера
+// Префикс и namespace можно было указать и так
+/*
+    Route::resource('blog/posts', 'Blog/PostController')->names('blog.posts');
+*/
+
+//Админка Блога
+// url'om будет 'admin/blog/' + 'categories/' из блока Route::resource
+// проверить можно будет командой php artisan route:list
+// новая папка Blog\Admin создаётся при создании контроллера (12.)
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+
+    //BlogCategory
+    // только эти методы будут задействованы, несмотря на 
+    // создание ресурсом в контроллере всех методов по умолчанию (+ show, + destroy)
+    $methods = ['index', 'edit', 'update', 'create', 'store'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+
+     //BlogPosts
+     // использовать все методы, кроме show
+     Route:: resource('posts', 'PostController')
+     ->except(['show'])
+     ->names('blog.admin.posts');    
+});
